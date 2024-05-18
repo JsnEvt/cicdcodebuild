659d77c026659ae2ec3e4275bc8b37de55704884
Jason Santos <110934550+JsnEvt@users.noreply.github.com>
2024-05-18 07:45:35 -0300

    Arquivos iniciais

---
+++ client/src/components/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+import Button from "./Button";
+
+const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation();
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      {/* <h3>process.env.EMAIL_ALUNO</h3> */}
+      {location.pathname === "/" && (
+        <Button
+          color={showAdd ? "red" : "green"}
+          text={showAdd ? "Close" : "Add"}
+          onClick={onAdd}
+        />
+      )}
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  title: "BIA 2024 - DESAFIO 1 CI/CD - ECS - CONCLUIDO!!!",
+};
+
+export default Header;
